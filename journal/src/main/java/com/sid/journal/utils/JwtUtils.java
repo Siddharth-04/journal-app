964c2799e2c0c8d5c79f58573e8a0e08886df976
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-04-21 14:00:57 +0530

    [Siddharth] ADD: jwt integration in login api

--- journal/src/main/java/com/sid/journal/utils/JwtUtils.java
+++ journal/src/main/java/com/sid/journal/utils/JwtUtils.java
@@ -1,4 +1,71 @@
 package com.sid.journal.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class JwtUtils {
+
+    private String SECRET_KEY = "secretKeyhaibhaiiseysambhalkerakhio";
+
+    public  String generateToken(String username){
+        Map<String,Object> claims = new HashMap<>();
+        return createToken(claims,username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .claims(claims)
+                .subject(subject)
+                .header().empty().add("typ","JWT")
+                .and()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000*6*60))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    private String extractUsername(String token){
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public Date extractExpiration(String token){
+        return extractAllClaims(token).getExpiration();
+    }
+
+    public Boolean validateToken(String token){
+        return !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception{
+//        return super().authenticationManagerBean();
+//    }
+
 }
